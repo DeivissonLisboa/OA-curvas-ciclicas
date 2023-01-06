6cda282f6915a67ec8b78ee1a26124d5d7ec1e92
Deivisson Lisboa <dvilaflor@gmail.com>
2023-01-06 12:53:44 -0300

    Criar botão que vai para o topo da página

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,19 @@
 const container = document.querySelector(".container")
 const progress = document.getElementById("progress-bar")
+const upBtn = document.querySelector(".up")
 
 window.addEventListener("scroll", () => {
   progress.style.width = `${
     (this.scrollY / (container.scrollHeight - this.innerHeight)) * 100
   }%`
+
+  if (window.scrollY > 0) {
+    upBtn.style.opacity = 0.7
+  } else {
+    upBtn.style.opacity = 0
+  }
+})
+
+upBtn.addEventListener("click", () => {
+  window.scrollTo(0, 0)
 })
